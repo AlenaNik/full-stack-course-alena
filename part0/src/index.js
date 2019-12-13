ad4cc1e9ae42b5b9caf29f20b8bd41b1018439d4
AlenaNik <alena.nik.digital@gmail.com>
2019-12-13 21:59:22 +0100

    Event handlers are functions

--- part0/src/index.js
+++ part0/src/index.js
@@ -1,29 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Header';
 import Total from './Total';
 import Content from './Content';
 
-const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+const App = (props) => {
+    const [counter, setCounter] = useState(0)
+
+  const setValueTo = (value) => setCounter(value);
+
 
     return (
-        <div>
-            <Header course={course}/>
-            <Content part1={part1} exercise1={exercises1}
-                     part2={part2} exercise2={exercises2}
-                     part3={part3} exercise3={exercises3}
-
-            />
-            <Total exercises={exercises1 + exercises2 + exercises3} />
+        <div>{counter}
+        <button onClick={() => setValueTo(counter + 1)}> Click me</button>
+        <button onClick={() => setValueTo(0)}> Click me to reset</button>
         </div>
-    )
+            )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
