8276d0f931a049b1a3206953d9147345762d8ec9
AlenaNik <alena.nik.digital@gmail.com>
2020-01-02 23:22:54 +0500

    use effect axios

--- part0/src/App.js
+++ part0/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Persons from './components/Persons.component'
 import Filter from "./components/Filter.component";
 import PersonFrom from './components/PersonForm.component';
+import axios from 'axios';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456'},
-        {name: 'Ada Lovelace', number: '39-44-5323523'},
-        {name: 'Dan Abramov', number: '12-43-234345'},
-        {name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+        axios.get('http://localhost:3001/persons')
+            .then(res => {
+                setPersons(res.data)
+            })
+    }, []);
 
     const addPerson = e => {
         e.preventDefault()
