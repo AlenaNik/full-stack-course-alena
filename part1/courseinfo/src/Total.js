a828b0398e0f41d8b9ca217d00e57e73349c5f85
AlenaNik <alena.nik.digital@gmail.com>
2020-03-18 22:51:24 +0100

    1.5

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 
 export const Total = (props) => {
+
+    const total = props.parts.reduce((acc, curr) => {
+        return acc += curr.exercises
+    }, 0);
+
     return(
         <>
-            <p>Number of exercises {props.total}</p>
+            <p>Number of exercises {total}</p>
         </>
     )
 }
