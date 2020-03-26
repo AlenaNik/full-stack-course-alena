91709e695057ca37d8b23f10ef32b165f48a2dc4
AlenaNik <alena.nik.digital@gmail.com>
2020-03-26 14:37:51 +0100

    part 2 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 import { useFormFields } from './FormHook'
 import { PersonForm } from './PersonForm'
@@ -6,12 +7,7 @@ import { Persons } from './Persons'
 import { Filter } from './Filter'
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [ persons, setPersons ] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ filterPersons, setFilterPersons ] = useState('')
@@ -20,6 +16,14 @@ const App = () => {
         number: '',
         filter: ''
     });
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons').then(response => {
+            setPersons(response.data)
+        })
+    }, [])
+
 
     const addPerson = (e) => {
         e.preventDefault()
